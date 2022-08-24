48c7ebcef1f106238de8d044651ededbe3716b90
NataliiaPasaman <natapasaman@gmail.com>
2022-08-24 23:08:15 +0300

    create gallery - html

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+/** Створи галерею з можливістю кліку по її елементах і 
+ * перегляду повнорозмірного зображення у модальному вікні.
+ *  Подивися демо відео роботи галереї.
+
+
+Реалізація делегування на div.gallery і отримання url 
+великого зображення. 
+ */
+
+
+
+// 1. Створення галереї зображень
+const divGallery = document.querySelector('.gallery');
+createImgGallery(galleryItems);
+
+function createImgGallery (array) {
+    array.forEach(({ preview, original, description }) => {
+        const imgElement = document.createElement('img');
+    
+        imgElement.dataSource = original;
+        imgElement.src = preview;
+        imgElement.alt = description;
+    
+        divGallery.appendChild(imgElement);
+    })
+}
+
+
+
