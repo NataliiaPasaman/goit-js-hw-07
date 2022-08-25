53bc3f2290d70346e49982273858ce22c6c8e24d
NataliiaPasaman <natapasaman@gmail.com>
2022-08-25 23:44:00 +0300

    модалка

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,40 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
 /** Створи галерею з можливістю кліку по її елементах і 
  * перегляду повнорозмірного зображення у модальному вікні.
  *  Подивися демо відео роботи галереї.
 
+4/ Відкриття модального вікна по кліку на елементі галереї.
 
-
-Підключення скрипту і стилів бібліотеки модального вікна 
-basicLightbox. Використовуй CDN сервіс jsdelivr і додай
-у проект посилання на мініфіковані (.min) файли бібліотеки.
+5/ Заміна значення атрибута src елемента <img> в 
+модальному вікні перед відкриттям. Використовуй готову 
+розмітку модального вікна із зображенням з прикладів 
+бібліотеки basicLightbox.
  */
 
-
-
 // 1. Створення галереї зображень
-const divGallery = document.querySelector('.gallery');
-createImgGallery(galleryItems);
+const divGallery = document.querySelector(".gallery");
 
-function createImgGallery (array) {
-    array.forEach(({ preview, original, description }) => {
-        const imgElement = document.createElement('img');
-    
-        imgElement.dataSource = original;
-        imgElement.src = preview;
-        imgElement.alt = description;
-    
-        divGallery.appendChild(imgElement);
-    })
-}
+const imgGalleryNew = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+  })
+  .join(" ");
 
+divGallery.insertAdjacentHTML("beforeend", imgGalleryNew);
 
 // 2. Делегування на div.gallery
 divGallery.addEventListener('click', onClickGallery)
 
 function onClickGallery (event) {
-    const urlOriginal = event.target.dataSource;
+    event.preventDefault()
+
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    };
+    const urlOriginal = event.target.dataset.source;
     console.log('URL original', urlOriginal);
-  
+
+    // 3. Модалка з бібліотеки basicLightbox
+    let instance = basicLightbox.create(`
+    <img class="gallery__image" src="${urlOriginal}"/>
+`)
+instance.show()
 }
+
+
