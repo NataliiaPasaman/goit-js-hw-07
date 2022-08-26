dbae103f0bc787c2c3c41e8106625c6a54d3c766
NataliiaPasaman <natapasaman@gmail.com>
2022-08-26 21:57:10 +0300

    створення розмітки

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+
+/** */
+
+const listGallery = document.querySelector(".gallery");
+
+const itemsGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+         <a class="gallery__item" href="${original}">
+           <img
+             class="gallery__image"
+             src="${preview}"
+             data-source="${original}"
+             alt="${description}"
+           />
+         </a>
+         `;
+  })
+  .join(" ");
+
+listGallery.insertAdjacentHTML("beforeend", itemsGallery);
